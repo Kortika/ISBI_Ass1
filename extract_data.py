4db5043c685c788f2df8047ee27963e1939c3015
VMormoris <vmormoris2014@gmail.com>
2019-11-13 15:18:20 +0200

    Adding percision Logic

--- extract_data.py
+++ extract_data.py
@@ -1,4 +1,4 @@
-
+import os
 def readfile(filename):
     name=filename.split('.csv')[0]
     question, engine=name.split('-')[0], name.split('-')[1]
@@ -14,7 +14,33 @@ def readfile(filename):
             data.append((line.split(',')[0], int(line.split(',')[1])))
     return (question, engine, data)
 
+def readData(questions, engines, dirpath='./data'):
+    filenames=[]
+    for r, d, f in os.walk(dirpath):
+        for file in f:
+            filenames.append(file)
+    unusefullData=[]
+    for filename in filenames:
+        unusefullData.append(readfile(dirpath+'/'+filename))
+    commonpools=([], [], [])
+    for i in range(0, engines):
+        for result in unusefullData[questions*i:questions*(i+1)]:
+            (question, engine, data)=result
+            divider=1
+            revs=0
+            for (link, relevance) in data:
+                if relevance==1 and not (link in commonpools[i]):
+                    commonpools[i].append(link)
+                if relevance==1:
+                    revs+=1
+                print(revs, '/', divider)
+                divider+=1
+            print('\n')
+
+readData(2, 2)#dirpath='C:/Users/Μαρία/Desktop/ISBI/ISBI_Ass1/data')
 #Printing Test
+'''
 (q, engine, data)=readfile('q4-google.csv')
 for element in data:
     print(element)
+'''
