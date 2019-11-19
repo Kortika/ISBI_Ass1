760c232a55688e4b308c12d0b2e49922a1289d95
Kortika (Sitt Min Oo) <sittminoo1995145@gmail.com>
2019-11-19 14:07:38 +0100

    fix error in graphing

--- graphploting.R
+++ graphploting.R
@@ -1,5 +1,5 @@
 #Initializations
-dirpath='C:/Users/Ìáñßá/Desktop/ISBI/ISBI_Ass1/pyresults/'#Specify project directory
+dirpath='C:/Users/ï¿½ï¿½ï¿½ï¿½ï¿½/Desktop/ISBI/ISBI_Ass1/pyresults/'#Specify project directory
 filenames=c('google.csv.result', 'duckduckgo.csv.result', 'webcrawler.csv.result')
 #Read Inputs
 google.frame=read.table(file=paste(dirpath, filenames[1], sep=""), header = TRUE, sep=",", na.strings="NA", strip.white = TRUE, stringsAsFactors = FALSE)
@@ -15,17 +15,17 @@ na.omit(webcrawler.frame)
 plot(google.frame$q1_recall, google.frame$q1_precision, type='o', col='blue', ylim=c(0,1), xlim=c(0,1), main='Google', xlab='Recall', ylab='precision')
 lines(google.frame$q2_recall, google.frame$q2_precision, type='o', col='red')
 lines(google.frame$q3_recall, google.frame$q3_precision, type='o', col='green')
-lines(google.frame$q3_recall, google.frame$q4_precision, type='o', col='yellow')
+lines(google.frame$q4_recall, google.frame$q4_precision, type='o', col='yellow')
 legend('bottomright', legend=c('q1', 'q2','q3','q4'), fill=c('blue', 'red', 'green', 'yellow'))
 #Plot graph for duckduckgo
 plot(duckduckgo.frame$q1_recall, duckduckgo.frame$q1_precision, type='o', col='blue', ylim=c(0,1), xlim=c(0,1), main='DuckDuckGo', xlab='Recall', ylab='precision')
 lines(duckduckgo.frame$q2_recall, duckduckgo.frame$q2_precision, type='o', col='red')
 lines(duckduckgo.frame$q3_recall, duckduckgo.frame$q3_precision, type='o', col='green')
-lines(duckduckgo.frame$q3_recall, duckduckgo.frame$q4_precision, type='o', col='yellow')
+lines(duckduckgo.frame$q4_recall, duckduckgo.frame$q4_precision, type='o', col='yellow')
 legend('bottomright', legend=c('q1', 'q2','q3','q4'), fill=c('blue', 'red', 'green', 'yellow'))
 #Plot graph for webcrawler
 plot(webcrawler.frame$q1_recall, webcrawler.frame$q1_precision, type='o', col='blue', ylim=c(0,1), xlim=c(0,1), main='Webcrawler', xlab='Recall', ylab='precision')
 lines(webcrawler.frame$q2_recall, webcrawler.frame$q2_precision, type='o', col='red')
 lines(webcrawler.frame$q3_recall, webcrawler.frame$q3_precision, type='o', col='green')
-lines(webcrawler.frame$q3_recall, webcrawler.frame$q4_precision, type='o', col='yellow')
+lines(webcrawler.frame$q4_recall, webcrawler.frame$q4_precision, type='o', col='yellow')
 legend('bottomright', legend=c('q1', 'q2','q3','q4'), fill=c('blue', 'red', 'green', 'yellow'))
