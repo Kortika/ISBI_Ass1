7d2b91222f2f28e9f7d0add022c2b832783a3a71
VMormoris <vmormoris2014@gmail.com>
2019-11-19 17:21:37 +0200

    Creating the last graph

--- README.md
+++ README.md
@@ -1,3 +1,36 @@
+# First Assignment in course Internet Search Techniques and Business Intelligence (2019-2020)<br>
+__Group Members:__<br>
+  * Ignatius Patrick
+  * Dare Gbenga Christopher Akinboyewa
+  * [Sitt Min Oo](https://github.com/Kortika)
+  * [Vasileios Mormoris](https://github.com/VMormoris)
 
+
+We look 4 queries in 3 different search engines. For each search engine we check for the top 30 suggestions that each made the relevance to the respective query. After that we calculated the precision and recall and the Average interpolated.
+
+## Queries
+
+  * __q1:__ _Fall of the Soviet Union_
+  * __q2:__ _What is the tinder algorithm?_
+  * __q3:__ _What is the population of Sierra Leone?_
+  * __q4:__ _How banks transfer money?_
+
+## Search Engines
+
+  * [Google](https://www.google.com)
+  * [DuckDuckGo](https://www.duckduckgo.com)
+  * [Webcrawler](https://www.webcrawler.com)
+
+## Results(Graphs)
+
+![Google](./graphs/Google_PR.png)<br><br>
+
+![DuckDuckGo](./graphs/DuckDuckGo_PR.png)<br><br>
+
+![Webcrawler](./graphs/Webcrawler_PR.png)<br><br>
+
+![Interpolated](./graphs/Interpolated.png)
+
+## Notes about the source code
 __Currently working directory in R is not the same with the directory that the file is located.__
 _Change dirpath value on graphloting.R in order for the R script to work properly._
--- graphploting.R
+++ graphploting.R
@@ -1,31 +1,46 @@
 #Initializations
-dirpath='C:/Users/ï¿½ï¿½ï¿½ï¿½ï¿½/Desktop/ISBI/ISBI_Ass1/pyresults/'#Specify project directory
-filenames=c('google.csv.result', 'duckduckgo.csv.result', 'webcrawler.csv.result')
+dirpath='C:/Users/Ìáñßá/Desktop/ISBI/ISBI_Ass1/pyresults/'#Specify project directory
+filenames=c('google.csv.result', 'duckduckgo.csv.result', 'webcrawler.csv.result', 'google_interpolated.csv.result', 'duckduckgo_interpolated.csv.result', 'webcrawler_interpolated.csv.result')
 #Read Inputs
-google.frame=read.table(file=paste(dirpath, filenames[1], sep=""), header = TRUE, sep=",", na.strings="NA", strip.white = TRUE, stringsAsFactors = FALSE)
-duckduckgo.frame=read.table(file=paste(dirpath, filenames[2], sep=""), header = TRUE, sep=",", na.strings="NA", strip.white = TRUE, stringsAsFactors = FALSE)
-webcrawler.frame=read.table(file=paste(dirpath, filenames[3], sep=""), header = TRUE, sep=",", na.strings="NA", strip.white = TRUE, stringsAsFactors = FALSE)
+google.prcsn_rcl.frame=read.table(file=paste(dirpath, filenames[1], sep=""), header = TRUE, sep=",", na.strings="NA", strip.white = TRUE, stringsAsFactors = FALSE)
+google.interpolated.frame=read.table(file=paste(dirpath, filenames[4], sep=""), header = TRUE, sep=",", na.strings="0.0", strip.white = TRUE, stringsAsFactors = FALSE)
+duckduckgo.prcsn_rcl.frame=read.table(file=paste(dirpath, filenames[2], sep=""), header = TRUE, sep=",", na.strings="NA", strip.white = TRUE, stringsAsFactors = FALSE)
+duckduckgo.interpolated.frame=read.table(file=paste(dirpath, filenames[5], sep=""), header = TRUE, sep=",", na.strings="0.0", strip.white = TRUE, stringsAsFactors = FALSE)
+webcrawler.prcsn_rcl.frame=read.table(file=paste(dirpath, filenames[3], sep=""), header = TRUE, sep=",", na.strings="NA", strip.white = TRUE, stringsAsFactors = FALSE)
+webcrawler.interpolated.frame=read.table(file=paste(dirpath, filenames[6], sep=""), header = TRUE, sep=",", na.strings="0.0", strip.white = TRUE, stringsAsFactors = FALSE)
 
 
 #Clear NA values
-na.omit(google.frame)
-na.omit(duckduckgo.frame)
-na.omit(webcrawler.frame)
+na.omit(google.prcsn_rcl.frame)
+na.omit(google.interpolated.frame)
+na.omit(duckduckgo.prcsn_rcl.frame)
+na.omit(duckduckgo.interpolated.frame)
+na.omit(webcrawler.prcsn_rcl.frame)
+na.omit(webcrawler.interpolated.frame)
+
 #Plot graph for Google
-plot(google.frame$q1_recall, google.frame$q1_precision, type='o', col='blue', ylim=c(0,1), xlim=c(0,1), main='Google', xlab='Recall', ylab='precision')
-lines(google.frame$q2_recall, google.frame$q2_precision, type='o', col='red')
-lines(google.frame$q3_recall, google.frame$q3_precision, type='o', col='green')
-lines(google.frame$q4_recall, google.frame$q4_precision, type='o', col='yellow')
+plot(google.prcsn_rcl.frame$q1_recall, google.prcsn_rcl.frame$q1_precision, type='o', col='blue', ylim=c(0,1), xlim=c(0,1), main='Google', xlab='Recall', ylab='precision')
+lines(google.prcsn_rcl.frame$q2_recall, google.prcsn_rcl.frame$q2_precision, type='o', col='red')
+lines(google.prcsn_rcl.frame$q3_recall, google.prcsn_rcl.frame$q3_precision, type='o', col='green')
+lines(google.prcsn_rcl.frame$q4_recall, google.prcsn_rcl.frame$q4_precision, type='o', col='yellow')
 legend('bottomright', legend=c('q1', 'q2','q3','q4'), fill=c('blue', 'red', 'green', 'yellow'))
+
 #Plot graph for duckduckgo
-plot(duckduckgo.frame$q1_recall, duckduckgo.frame$q1_precision, type='o', col='blue', ylim=c(0,1), xlim=c(0,1), main='DuckDuckGo', xlab='Recall', ylab='precision')
-lines(duckduckgo.frame$q2_recall, duckduckgo.frame$q2_precision, type='o', col='red')
-lines(duckduckgo.frame$q3_recall, duckduckgo.frame$q3_precision, type='o', col='green')
-lines(duckduckgo.frame$q4_recall, duckduckgo.frame$q4_precision, type='o', col='yellow')
+plot(duckduckgo.prcsn_rcl.frame$q1_recall, duckduckgo.prcsn_rcl.frame$q1_precision, type='o', col='blue', ylim=c(0,1), xlim=c(0,1), main='DuckDuckGo', xlab='Recall', ylab='precision')
+lines(duckduckgo.prcsn_rcl.frame$q2_recall, duckduckgo.prcsn_rcl.frame$q2_precision, type='o', col='red')
+lines(duckduckgo.prcsn_rcl.frame$q3_recall, duckduckgo.prcsn_rcl.frame$q3_precision, type='o', col='green')
+lines(duckduckgo.prcsn_rcl.frame$q4_recall, duckduckgo.prcsn_rcl.frame$q4_precision, type='o', col='yellow')
 legend('bottomright', legend=c('q1', 'q2','q3','q4'), fill=c('blue', 'red', 'green', 'yellow'))
+
 #Plot graph for webcrawler
-plot(webcrawler.frame$q1_recall, webcrawler.frame$q1_precision, type='o', col='blue', ylim=c(0,1), xlim=c(0,1), main='Webcrawler', xlab='Recall', ylab='precision')
-lines(webcrawler.frame$q2_recall, webcrawler.frame$q2_precision, type='o', col='red')
-lines(webcrawler.frame$q3_recall, webcrawler.frame$q3_precision, type='o', col='green')
-lines(webcrawler.frame$q4_recall, webcrawler.frame$q4_precision, type='o', col='yellow')
+plot(webcrawler.prcsn_rcl.frame$q1_recall, webcrawler.prcsn_rcl.frame$q1_precision, type='o', col='blue', ylim=c(0,1), xlim=c(0,1), main='Webcrawler', xlab='Recall', ylab='precision')
+lines(webcrawler.prcsn_rcl.frame$q2_recall, webcrawler.prcsn_rcl.frame$q2_precision, type='o', col='red')
+lines(webcrawler.prcsn_rcl.frame$q3_recall, webcrawler.prcsn_rcl.frame$q3_precision, type='o', col='green')
+lines(webcrawler.prcsn_rcl.frame$q4_recall, webcrawler.prcsn_rcl.frame$q4_precision, type='o', col='yellow')
 legend('bottomright', legend=c('q1', 'q2','q3','q4'), fill=c('blue', 'red', 'green', 'yellow'))
+
+#Plot intepolation Graph
+plot(google.interpolated.frame$std_recall, google.interpolated.frame$average, type='o', col='blue', ylab='Precission', ylim=c(0, 1), xlim=c(0, 1), main='Average Interpolated', xlab='Recall' )
+lines(duckduckgo.interpolated.frame$std_recall, duckduckgo.interpolated.frame$average, type='o', col='red')
+lines(webcrawler.interpolated.frame$std_recall, webcrawler.interpolated.frame$average, type='o', col='green')
+legend('topright', legend=c('Google', 'DuckDuckGo', 'Webcrawler'), fill=c('blue', 'red', 'green'))
